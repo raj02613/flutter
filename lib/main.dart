544d8b0b3192e5dffc6de6228127eba364925b5a
Raj Bahadur Yadav <raj02613@gmail.com>
2023-11-08 16:06:33 +0530

    Day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
+import 'package:classico/pages/home_page.dart';
+
+import 'package:flutter/material.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(
-        child: Text("Welcome to 30 days of flutter"),
-      ),
+      home: HomePage(),
     );
   }
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Rajjoo";
+  @override
+  Widget build(BuildContext Context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child:Container(
+          child: Text("Welcome to $days days of flutter by $name "),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
