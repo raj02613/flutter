1511241ca056230d03569afa83d5e63aa550219a
Raj Bahadur Yadav <raj02613@gmail.com>
2023-11-23 12:46:17 +0530

    Day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:classico/pages/home_page.dart';
+import 'package:classico/pages/login_page.dart';
 
 import 'package:flutter/material.dart';
-
-import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,8 +11,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
+    return  MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch:Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/",
+      routes: {
+        "/": (contex) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
